8f20856084edf7bb074d312473a3d9428d3775e2
sjood <sjood@NIKLASDATA>
2023-10-25 10:14:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Circle2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange radien på cirkeln vi ska beräkna: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            var myCircle = new Circle(radius);
+
+            double cArea = myCircle.GetArea();
+            double cCircumference = myCircle.GetCircumference();
+            double cVolume = myCircle.GetVolume();
+
+            Console.WriteLine("\nArean är " + Math.Round(cArea, 2) + " cm.");
+            Console.WriteLine("Omkretsen är " + Math.Round(cCircumference, 2) + " cm.");
+            Console.WriteLine("Volymen är " + Math.Round(cVolume, 2) + " kubik cm.");
+        }
+    }
+}
